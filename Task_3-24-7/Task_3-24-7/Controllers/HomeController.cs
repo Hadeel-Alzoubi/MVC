d43710c5de277ce07edf6074aae5778039f6a6e5
Hadeel-Alzoubi <hadeelzoubi12@gmail.com>
2024-07-25 07:17:16 +0300

    task 3

---
+++ Task_3-24-7/Task_3-24-7/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Task_3_24_7.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Contact(FormCollection form)
+        {
+            string name = form["yourname"];
+            string email = form["youremail"];
+            string massege = form["yourmsg"];
+
+            ViewBag["yourname"]=name;
+            ViewBag["youremail"] =email;
+            ViewBag["yourmsg"] = massege;
+            return View();
+        }
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Login(string email, string password)
+        {
+
+
+            if (email == "hadeelzoubi@gmail.com" && password == "123")
+            {
+                Session["UserName"] = "Hadeel";
+                return RedirectToAction("Home");
+            }
+            else
+            {
+                ViewBag.Message = "Invalid email or password";
+                return View();
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            // Clear session
+            Session["UserName"] = null;
+            return RedirectToAction("Home");
+        }
+    }
+}
---
+++ Task_3-24-7/Task_3-24-7/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Task_3_24_7.MvcApplication" Language="C#" %>
